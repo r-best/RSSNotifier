b4704ea97a256ac8762df26f952b71ddc0ecb6a9
bobby <bobbyisbest3@gmail.com>
2019-05-24 10:29:32 -0400

    Able to fetch last update time from RSS feed and display on screen

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+function fetchRSSUpdateTime(rssurl){
+	var http = new XMLHttpRequest();
+	
+	http.onreadystatechange = () => {
+		if(http.readyState == http.DONE){
+			console.log("HTTP DONE");
+			if(http.status == 200){
+				box = document.querySelector('#textbox');
+				
+				rss = new DOMParser().parseFromString(http.responseText, 'text/xml');
+				box.innerHTML = rss.getElementsByTagName('lastBuildDate')[0].innerHTML;
+			}
+			else{
+				alert("Error fetching RSS feed");
+			}
+		}
+	};
+	http.open("GET", rssurl);
+	http.send();
+}
 
 window.onload = function () {
     // TODO:: Do your initialization job
-
+	console.log("INITIALIZING APP");
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back")
@@ -9,6 +29,10 @@ window.onload = function () {
 	    tizen.application.getCurrentApplication().exit();
 	} catch (ignore) {
 	}
+    });
+    
+    document.addEventListener('rotarydetent', () => {
+    	fetchRSSUpdateTime('http://mbmbam.libsyn.com/rss');
     });
 
     // Sample code
