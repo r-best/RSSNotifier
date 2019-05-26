7ed3627fb7c368db82a8474c7b78d649a9409e82
bobby <bobbyisbest3@gmail.com>
2019-05-26 14:23:38 -0400

    Reorganizing to support new pages easier

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var SUBS = ["http://mbmbam.libsyn.com/rss", "http://adventurezone.libsyn.com/rss", "http://rosebuddies.libsyn.com/rss"];
+
 function fetchRSS(rssurl){
 	return new Promise((resolve, reject) => {
 		var http = new XMLHttpRequest();
@@ -24,59 +26,6 @@ function fetchRSS(rssurl){
 	});
 }
 
-var SUBS = ["http://mbmbam.libsyn.com/rss", "http://adventurezone.libsyn.com/rss", "http://rosebuddies.libsyn.com/rss"];
-
-// Helper function to create a list element
-function genListElement(sub){
-	// Initialize list element
-	var li = document.createElement('li');
-	li.rssurl = sub;
-	
-	// Add title & subtitle to list element with placeholder text
-	var title = document.createElement('div');
-	title.setAttribute("class", "ui-marquee ui-marquee-gradient");
-	title.innerHTML = "Loading";
-	li.appendChild(title);
-	
-	var subtitle = document.createElement('div');
-	subtitle.setAttribute("class", "ui-li-sub-text");
-	subtitle.innerHTML = "Loading";
-	li.appendChild(subtitle);
-	
-	return li;
-}
-
-function initSubsListPage(){
-    var page = document.getElementById("sublist");
-    var listHelper;
-
-	page.addEventListener("pageshow", () => {
-		var list = document.getElementById("subs");
-		while(list.firstChild) list.removeChild(list.firstChild); // Remove all current list elements
-
-		// Add a new default list element for each subscription
-		SUBS.forEach(sub => list.appendChild(genListElement(sub)));
-
-		// Asynchronously fetch the real title & subtitle text for each element, updating when available
-		new Promise(async(resolve, reject) => {
-			var node = list.childNodes[0];
-			while(true){
-				var rss = await fetchRSS(node.rssurl);
-				node.querySelector("div.ui-marquee.ui-marquee-gradient").innerHTML = rss.name;
-				node.querySelector("div.ui-li-sub-text").innerHTML = rss.lastUpdate;
-
-				if(node.nextSibling) node = node.nextSibling;
-				else break;
-			}
-			listHelper = tau.helper.SnapListMarqueeStyle.create(list, {marqueeDelay: 1000});
-		});
-	});
-
-	page.addEventListener("pagehide", () => {
-		listHelper.destroy();
-	});
-}
-
 window.onload = function(){
     // TODO:: Do your initialization job
 	console.log("INITIALIZING APP");
@@ -96,4 +45,5 @@ window.onload = function(){
 
 	// Initialize Subscription List Page
 	initSubsListPage();
+	initAddSubPage();
 };
