2695877299bb8ef55ad0386be32e600bb871a24e
bobby <bobbyisbest3@gmail.com>
2019-05-26 21:45:59 -0400

    Making NumberPicker way easier to use

--- js/utils/NumberPicker.js
+++ js/utils/NumberPicker.js
@@ -1,9 +1,6 @@
 /**
- * Tizen TAU Wearable 4.0 page that allows the user to select a number using the rotary dial.
- * Just include this script at the end of the body and either link to it with `<a href="number-picker">`
- * or call the static `pick()` method.
- * When the user selects their number and backs out of the page, the value will be stored in the `data`
- * attribute of an element with id `courier`, which must be present.
+ * Tizen TAU Wearable 4.0 page that allows the user to select a number using the rotary dial, 
+ * just include this script at the end of the body.
  */
 class NumberPicker extends HTMLElement{
 	constructor(){
@@ -29,7 +26,7 @@ class NumberPicker extends HTMLElement{
 		var btn = document.createElement('a');
 		btn.setAttribute('class', 'ui-btn');
 		btn.innerHTML = "Select";
-		btn.addEventListener('click', () => window.history.back())
+		btn.addEventListener('click', () => tau.back())
 		this.footer.appendChild(btn);
 
 		// Create rotary event handler function
@@ -42,23 +39,26 @@ class NumberPicker extends HTMLElement{
 	}
 
 	connectedCallback(){
-		// Set page attributes
-		this.setAttribute("id", "number-picker");
+		this.setAttribute("id", "number-picker-page");
 		this.setAttribute("class", "ui-page");
-		this.min = this.getAttribute('min');
-		if(typeof(this.min) === 'string') this.min = parseInt(this.min);
-		this.max = this.getAttribute('max');
-		if(typeof(this.max) === 'string') this.max = parseInt(this.max);
-		this.step = this.getAttribute('step') || 1;
-		this.value = this.getAttribute('value') || 0;
+		this.storeback_id = 'courier'; // id of the element to store the chosen number in when the user leaves the page
 
 		// Set event handlers
 		this.addEventListener('pageshow', () => {
+			// Set page attributes
+			console.log("getval", this.getAttribute('value'))
+			this.min = this.getAttribute('min');
+			if(typeof(this.min) === 'string') this.min = parseInt(this.min);
+			this.max = this.getAttribute('max');
+			if(typeof(this.max) === 'string') this.max = parseInt(this.max);
+			this.step = this.getAttribute('step') || 1;
+			this.display.innerHTML = this.getAttribute('value') || 0;
+
 			document.addEventListener('rotarydetent', this.rotaryHandler);
 		});
 		this.addEventListener("pagehide", () => {
 			document.removeEventListener('rotarydetent', this.rotaryHandler);
-			document.getElementById('courier').data = this.display.innerHTML;
+			document.getElementById(this.storeback_id).innerHTML = this.display.innerHTML;
 		});
 
 		// Assemble element
@@ -70,7 +70,7 @@ class NumberPicker extends HTMLElement{
 	increment(){
 		// -= is fine for decrement, but we need parseInt here because += thinks we're concatenating
 		if(this.max === null || this.display.innerHTML <= this.max-this.step)
-			this.display.innerHTML = parseInt(this.display.innerHTML) + this.step;
+			this.display.innerHTML = parseInt(this.display.innerHTML) + parseInt(this.step);
 	}
 
 	decrement(){
@@ -78,19 +78,50 @@ class NumberPicker extends HTMLElement{
 			this.display.innerHTML -= this.step;
 	}
 	
-	static pick(startVal, min, max, step){
-		if(startVal) this.setAttribute("value", startVal);
-		if(min) this.setAttribute("min", min);
-		if(max) this.setAttribute("max", max);
-		if(step) this.setAttribute("step", step);
-		tau.changePage("number-picker");
+	static pick(id, startVal, min, max, step){
+		var numberPicker = document.querySelector("#number-picker-page");
+		numberPicker.storeback_id = id;
+		console.log("startval", startVal);
+		if(startVal) numberPicker.setAttribute("value", startVal);
+		if(min) numberPicker.setAttribute("min", min);
+		if(max) numberPicker.setAttribute("max", max);
+		if(step) numberPicker.setAttribute("step", step);
+		tau.changePage("#number-picker-page");
 	}
 }
 
-// Define the element
-customElements.define('number-picker', NumberPicker);
-// Add an instance of the element to the body
-document.getElementsByTagName("body")[0].appendChild(document.createElement("number-picker"));
+/**
+ * A customized version of the HTML input element that uses the NumberPicker page defined above. Simply
+ * place it in your HTML and when the user clicks on it it will open the NumberPicker, allowing them to
+ * choose their value.
+ * When the user selects their number and backs out of the page, the value will be stored in the innerHTML 
+ * of this element.
+ */
+class NumberPickerActivator extends HTMLElement{
+	constructor(){
+		super();
+		this.addEventListener('click', () => {
+			console.log("innerhtml", this.innerHTML)
+			NumberPicker.pick(
+				this.id,
+				this.innerHTML,
+				this.getAttribute('min'),
+				this.getAttribute('max'),
+				this.getAttribute('step')
+			);
+		});
+	}
+
+	connectedCallback(){
+		if(!this.getAttribute('id')) this.setAttribute('id', 'courier');
+	}
+}
+
+// Define the elements
+customElements.define('number-picker-page', NumberPicker);
+customElements.define('number-picker', NumberPickerActivator);
+// Add an instance of the NumberPicker to the body
+document.getElementsByTagName("body")[0].appendChild(document.createElement("number-picker-page"));
 // Create a courier to carry the number result back if the user doesn't already have one
 if(!document.getElementById('courier')){
 	var courier = document.createElement("div");
